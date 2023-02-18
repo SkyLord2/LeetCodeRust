42d30bed871d45e31a6ad31757b9b29e66738ae2
SkyLord2 <chengdongsheng@outlook.com>
2023-02-18 22:09:52 +0800

    链表

--- src/main.rs
+++ src/main.rs
@@ -23,6 +23,69 @@ pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
     result
 }
 
+/*
+19. 删除链表的倒数第 N 个结点
+给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。
+*/
+// Definition for singly-linked list.
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+  pub val: i32,
+  pub next: Option<Box<ListNode>>
+}
+
+impl ListNode {
+  #[inline]
+  fn new(val: i32) -> Self {
+    ListNode {
+      next: None,
+      val
+    }
+  }
+}
+pub fn remove_nth_from_end(head: Option<Box<ListNode>>, n: i32) -> Option<Box<ListNode>> {
+    head
+}
+
+/*
+21. 合并两个有序链表
+将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+*/
+pub fn merge_two_lists(list1: Option<Box<ListNode>>, list2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+    list1
+}
+
+/*
+23. 合并K个升序链表
+给你一个链表数组，每个链表都已经按升序排列。
+请你将所有链表合并到一个升序链表中，返回合并后的链表。
+*/
+pub fn merge_k_lists(lists: Vec<Option<Box<ListNode>>>) -> Option<Box<ListNode>> {
+    if lists.len() == 0 {
+        return None;
+    } else {
+        lists[0].clone()
+    }
+}
+
+/*
+86. 分隔链表
+给你一个链表的头节点 head 和一个特定值 x ，请你对链表进行分隔，使得所有 小于 x 的节点都出现在 大于或等于 x 的节点之前。
+你应当 保留 两个分区中每个节点的初始相对位置。
+*/
+pub fn partition(head: Option<Box<ListNode>>, x: i32) -> Option<Box<ListNode>> {
+    head
+}
+
+/*
+876. 链表的中间结点
+给定一个头结点为 head 的非空单链表，返回链表的中间结点。
+如果有两个中间结点，则返回第二个中间结点。
+*/
+pub fn middle_node(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+
+}
+
 fn main() {
     let nums = vec![3,2,4];
     let target = 6;
